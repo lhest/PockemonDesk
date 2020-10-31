64793b47a64d4a6b3d8f92d882011137bacdd216
yrul <iuliia.rul@gmail.com>
2020-10-31 12:19:59 +0200

    seems done, the * task not

--- src/example.ts
+++ src/example.ts
@@ -3,3 +3,23 @@ type StringConc = string;
 const concat = (a: StringConc, b: StringConc): StringConc => a + b;
 
 console.log(concat('Hello ', 'World'));
+
+
+
+interface MyInterface {
+    howIDoIt: string;
+    simeArray: [number | string];
+    withData?: [MyInterface];
+}
+
+const MyHometask = {
+    howIDoIt: "I Do It Wel",
+    simeArray: ["string one", "string two", 42],
+    withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+}
+
+interface MyArray<T> {
+    [N: number]: T;
+
+    reduce<U>(callbackFn: (prevValue: U, currentValue: T, currentIndex: number, array: T[]) => U, initValue:U): U;
+}
