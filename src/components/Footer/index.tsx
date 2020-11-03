6266cfda604d40892d436a40a305b53f8bbaddd7
yrul <iuliia.rul@gmail.com>
2020-11-03 22:03:18 +0200

    WIP the beginning without styles yet

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer >
+            <p >
+                &copy; Make with Love &middot; November &middot; 2020
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
