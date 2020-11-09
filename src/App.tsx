a25281ed939937e0591a7ff077d5b05bd10dd803
yrul <iuliia.rul@gmail.com>
2020-11-10 00:46:06 +0200

    header and homepage to git

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import cn from 'classnames';
 
 // import s from './App.module.scss';
-import Header from './components/Header';
+import HomePage from "./pages/Home";
 
 const App = () => {
   console.log('###: Some Log');
-  return <Header />;
+  return <HomePage />;
 };
 
 export default App;
