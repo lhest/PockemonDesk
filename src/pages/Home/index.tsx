6c45c82f712db910ca27e3bb90776e2887fc2696
yrul <iuliia.rul@gmail.com>
2020-11-10 16:21:18 +0200

    button added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from "../../components/Header";
+import Layout from "../../components/Layout";
+import Button from "../../components/Button";
 
 import s from "./Home.module.scss";
 
@@ -7,6 +9,16 @@ const HomePage = () => {
     return (
         <div className={s.root}>
             <Header />
+            <Layout className={s.contentWrap}>
+                <div className={s.contentText}>
+                    <h1>
+                        <b>Find</b> all your favourite <b>Pokemon</b>
+                    </h1>
+                    <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
+                    <Button onClick={() => console.log("this button pushed")}>See Pokemons</Button>
+                </div>
+                <div className={s.contentParallax}> We&apos;ll see the Parallax here soon... </div>
+            </Layout>
         </div>
     );
 };
